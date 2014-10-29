2502606f446d000f0e58c683dceb83e1ac3f40d1
GuilhermeAlmeida <GuilhermeAlmeida@GUI>
2014-10-28 22:17:51 -0200

    Consulta PlanoContasConsulta criada com sucesso !

--- src/main/java/br/com/munif/masterfinance2/aplicacao/Programa.java
+++ src/main/java/br/com/munif/masterfinance2/aplicacao/Programa.java
@@ -6,6 +6,7 @@
 package br.com.munif.masterfinance2.aplicacao;
 
 import br.com.munif.masterfinance2.desktop.ContaCorrenteConsulta;
+import br.com.munif.masterfinance2.desktop.PlanoContasConsulta;
 import br.com.munif.masterfinance2.entidades.Fisica;
 import java.util.Properties;
 import javax.persistence.EntityManager;
@@ -32,6 +33,8 @@ public class Programa {
         em.getTransaction().commit();
         
         new ContaCorrenteConsulta();
+        
+        new PlanoContasConsulta();
 
     }
 
